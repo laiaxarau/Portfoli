9683a54ac9ae6966ab64099f6eabe3aa9a4d4786
laiaxarau <laiaxarau@gmail.com>
2024-04-09 07:44:52 +0200

    feat: design sketch

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		"./node_modules/flowbite/**/*.js"],
+	theme: {
+		extend: {
+			colors: {
+				
+				'verd': '#032b40',
+				'lila': '#180340',
+				'granate': '#400322',
+				'mostassa': '#5c5104',
+				'rosa':'#5c0430',
+				'verdplom':'#043d5c',
+
+				'fondo': '#355070',
+				'fondo2': '#1d273f',
+				'headers':'#243c5a',
+				'lletra':'#d3d8dc',
+				'lletra2':'#245a3c',
+
+
+			}
+		},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
