2a2fde9312553b573e7bb03c85fa6b5ba9b51ffe
laiaxarau <laiaxarau@gmail.com>
2024-04-14 17:55:04 +0200

    new design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,7 @@ export default {
 			colors: {
 				
 				'verd': '#032b40',
-				// 'verd': '#29607D',
+				'verdclar': '#29607D',
 				// 'verd':'#646A6D',
 				'lila': '#180340',
 				'granate': '#400322',
@@ -18,7 +18,7 @@ export default {
 				'fondo': '#355070',
 				'fondo2': '#1d273f',
 				'headers':'#243c5a',
-				'lletra':'#d3d8dc',
+				'lletra':'#333',
 				
 
 
